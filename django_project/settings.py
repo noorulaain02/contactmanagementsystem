863761b8402af0223a1dbf36a9d915db0e2b42a7
noorulaain02 <noorulaain34ghazi@gmail.com>
2025-05-15 13:26:14 +0500

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'office',
         'USER': 'root',
-        'PASSWORD': 'pretty02',
+        'PASSWORD': 'yourpassword',
         'HOST': 'localhost',
         'PORT': '3306',
     }
